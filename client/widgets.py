f67744de2367aa998e54d2f6869b0aef45cb37b3
RafaelKhammadeev <msmk.swimmer@gmail.com>
2022-12-05 12:59:26 +0300

    add widgets and some fix

---
+++ client/widgets.py
@@ -0,0 +1,63 @@
+import sys
+import socket
+from PyQt6 import uic
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QDialog, QStackedWidget
+
+
+# виджет авторизации
+class Authorization(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("design/authorization.ui", self)
+        self.btn.clicked.connect(self.switch_on_lobby)
+
+    # переключается на виджет лобби
+    @staticmethod
+    def switch_on_lobby():
+        lobby = Lobby()
+        widget.addWidget(lobby)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
+
+# виджет с лобби
+class Lobby(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("design/lobby.ui", self)
+        self.btn.clicked.connect(self.switch_on_game)
+
+    # переключение на виджет игры
+    @staticmethod
+    def switch_on_game():
+        game = Game()
+        widget.addWidget(game)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
+
+# Основной экран с игрой
+class Game(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("design/game.ui", self)
+        self.btn.clicked.connect(self.switch_on_lobby)
+
+    # переключение на виджет лобби
+    @staticmethod
+    def switch_on_lobby():
+        lobby = Lobby()
+        widget.addWidget(lobby)
+        widget.setCurrentIndex(widget.currentIndex() - 1)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    widget = QStackedWidget()
+    authorization = Authorization()
+    lobby = Lobby()
+    game = Game()
+    widget.addWidget(authorization)
+    widget.addWidget(lobby)
+    widget.addWidget(game)
+    widget.setFixedSize(500, 500)
+    widget.show()
+    sys.exit(app.exec())
